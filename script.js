551f485b32c72f0bd442f91ff342e54769863e68
David Rivera A <davidriveraarbelaez@gmail.com>
2024-10-03 20:27:14 -0500

    API

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiKey = '31a5dfb57d397d72dd3d3a97cba7a69e'; // Copiar la API enviada al correo y pegarla aquí
+const cityInput = document.getElementById('cityInput');
+const searchButton = document.getElementById('searchButton');
+const cityElement = document.getElementById('city');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const city = cityInput.value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            cityElement.textContent = data.name;
+            temperatureElement.textContent = `${data.main.temp}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
